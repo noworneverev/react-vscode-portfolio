b3aa8007360e29cafd4ed96cb048bd9b2f857d89
Yan-Ying Liao <n9102125@gmail.com>
2022-10-07 15:30:19 +0200

    modify sidebar's hovering effect to make it look more like vscode

--- src/app/layout/Sidebar.tsx
+++ src/app/layout/Sidebar.tsx
@@ -24,7 +24,6 @@ export default function Sidebar({
     <Box
       sx={{
         height: `calc(100vh - 20px)`,
-        pt: 1,
         backgroundColor: darkMode ? "#333333" : "#2c2c2c",
       }}
       justifyContent="space-between"
@@ -41,57 +40,84 @@ export default function Sidebar({
         flexDirection="column"
       >
         <Box
-          sx={{ flexGrow: 0, m: 0.5 }}
-          display="flex"
-          justifyContent="center"
+          sx={{
+            borderLeft: expanded ? "solid 0.12em white" : "none",
+            cursor: "pointer",
+          }}
+          onClick={() => setExpanded(!expanded)}
         >
-          <IconButton
-            onClick={() => setExpanded(!expanded)}
-            sx={{ color: "white" }}
+          <Box
+            sx={{
+              flexGrow: 0,
+              my: 1.5,
+              color: expanded ? "white" : "#858585",
+              fontSize: 24,
+
+              "&:hover": {
+                color: "white",
+              },
+            }}
+            display="flex"
+            justifyContent="center"
           >
             <VscFiles />
-          </IconButton>
+          </Box>
         </Box>
-
-        <Box
-          sx={{ flexGrow: 0, m: 0.5 }}
-          display="flex"
-          justifyContent="center"
-        >
-          <Tooltip title="Source of this project" arrow placement="right">
-            <Link
-              target="_blank"
-              href={"https://github.com/noworneverev/react-vscode-portfolio"}
-              underline="none"
-              color="inherit"
+        <Tooltip title="Source of this project" arrow placement="right">
+          <Link
+            target="_blank"
+            href={"https://github.com/noworneverev/react-vscode-portfolio"}
+            underline="none"
+            color="inherit"
+          >
+            <Box
+              sx={{
+                flexGrow: 0,
+                cursor: "pointer",
+                color: "#858585",
+                fontSize: 24,
+                "&:hover": {
+                  color: "white",
+                },
+              }}
+              display="flex"
+              justifyContent="center"
             >
-              <IconButton sx={{ color: "white" }}>
+              <Box mt={0.7}>
                 <BiGitBranch />
-              </IconButton>
-            </Link>
-          </Tooltip>
-        </Box>
+              </Box>
+            </Box>
+          </Link>
+        </Tooltip>
 
         <Divider sx={{ m: 0.5 }} />
 
         {links.map((link) => (
-          <Box
-            key={link.index}
-            sx={{ flexGrow: 0, m: 0.5 }}
-            display="flex"
-            justifyContent="center"
-          >
-            <Tooltip title={link.title} arrow placement="right">
-              <Link
-                target="_blank"
-                href={link.href}
-                underline="none"
-                color="inherit"
+          <Tooltip title={link.title} arrow placement="right" key={link.index}>
+            <Link
+              target="_blank"
+              href={link.href}
+              underline="none"
+              color="inherit"
+            >
+              <Box
+                sx={{
+                  flexGrow: 0,
+                  m: 0.5,
+                  color: "#858585",
+                  fontSize: 24,
+                  "&:hover": {
+                    color: "white",
+                  },
+                  cursor: "pointer",
+                }}
+                display="flex"
+                justifyContent="center"
               >
-                <IconButton sx={{ color: "white" }}>{link.icon}</IconButton>
-              </Link>
-            </Tooltip>
-          </Box>
+                <Box mt={0.7}>{link.icon}</Box>
+              </Box>
+            </Link>
+          </Tooltip>
         ))}
       </Box>
 
@@ -101,33 +127,52 @@ export default function Sidebar({
         justifyContent="center"
         flexDirection="column"
       >
-        <Box sx={{ flexGrow: 0 }} display="flex" justifyContent="center">
-          {darkMode ? (
-            <Tooltip title="Turn on the light" placement="right" arrow>
-              <IconButton
-                onClick={handleThemeChange}
-                color="inherit"
-                sx={{ color: "white" }}
-              >
+        <Tooltip
+          title={darkMode ? "Turn on the light" : "Turn off the light"}
+          placement="right"
+          arrow
+        >
+          <Box
+            sx={{
+              flexGrow: 0,
+              fontSize: 24,
+              color: "#858585",
+              cursor: "pointer",
+              "&:hover": {
+                color: "white",
+              },
+            }}
+            display="flex"
+            justifyContent="center"
+            onClick={handleThemeChange}
+          >
+            {!darkMode ? (
+              <Box>
                 <DarkModeOutlinedIcon />
-              </IconButton>
-            </Tooltip>
-          ) : (
-            <Tooltip title="Turn off the light" placement="right" arrow>
-              <IconButton
-                onClick={handleThemeChange}
-                color="inherit"
-                sx={{ color: "white" }}
-              >
+              </Box>
+            ) : (
+              <Box>
                 <LightModeOutlinedIcon />
-              </IconButton>
-            </Tooltip>
-          )}
-        </Box>
-        <Box sx={{ flexGrow: 0 }} display="flex" justifyContent="center">
-          <IconButton sx={{ color: "white" }}>
+              </Box>
+            )}
+          </Box>
+        </Tooltip>
+        <Box
+          sx={{
+            flexGrow: 0,
+            fontSize: 24,
+            color: "#858585",
+            cursor: "pointer",
+            "&:hover": {
+              color: "white",
+            },
+          }}
+          display="flex"
+          justifyContent="center"
+        >
+          <Box mt={0.7}>
             <VscSettingsGear />
-          </IconButton>
+          </Box>
         </Box>
       </Box>
     </Box>
